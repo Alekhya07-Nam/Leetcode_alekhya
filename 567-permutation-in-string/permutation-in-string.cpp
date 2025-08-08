2e4ff7ecffcc12c697bfd33758b32b04d5e44558
AlekhyaNam <alekhyanam@gmail.com>
2025-08-08 06:17:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (43.48%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size()>s2.size()){
+            return false;
+        }
+        vector<int> f1(26, 0), f2(26, 0);
+        for(int i=0;i<s1.size();i++){
+            f1[s1[i]-'a']++;
+        }
+        int n=s1.size();
+        for(int i=0;i<n;i++){
+            f2[s2[i]-'a']++;
+        }
+        if(f1==f2){
+            return true;
+        }
+        for(int i=n;i<s2.size();i++){
+            f2[s2[i]-'a']++;
+            f2[s2[i-n]-'a']--;
+            if(f1==f2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
