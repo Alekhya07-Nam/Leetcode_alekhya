ed390aee818898a3483856a2a902a7290b4f8d2d
AlekhyaNam <alekhyanam@gmail.com>
2025-12-24 09:55:16 +0530

    Time: 0 ms (100.00%) | Memory: 33 MB (92.95%) - LeetSync

---
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+       int sum=0;
+       for(int i=0;i<apple.size();i++){
+            sum+= apple[i];
+       } 
+       sort(capacity.begin(), capacity.end());
+       reverse(capacity.begin(), capacity.end());
+       int cnt=0;
+       for(int i=0;i<capacity.size();i++){
+            if(sum>0){
+                sum=sum-capacity[i];
+                cnt++;
+            }
+            else{
+                return cnt;
+            }
+       }
+       return cnt;
+    }
+};
