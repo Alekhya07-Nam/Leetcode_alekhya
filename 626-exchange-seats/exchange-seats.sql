e8ed7d94a693683b3a83633022b38e3555cf852c
AlekhyaNam <alekhyanam@gmail.com>
2026-02-01 21:03:17 +0530

    Time: 345 ms (41.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select id, 
+coalesce(
+case 
+    when id%2=0 then lag(student) over(order by id)
+    else lead(student) over(order by id)
+    end , student) as student 
+from seat;
